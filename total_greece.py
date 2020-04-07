0290e07e75b7879e4db3e6e7ac77a4972f02374b
AlanTurist <59668425+AlanTurist@users.noreply.github.com>
2020-04-07 23:47:11 +0200

    Update total_greece.py

--- total_greece.py
+++ total_greece.py
@@ -44,33 +44,29 @@ print("\n\t\t5.1 Σε κρίσιμη κατάσταση είναι το",'{0:.2f
 f = (100*D5)/247
 print("\n\t6. Είναι κατηλλημένο το",'{0:.2f}'.format(f),"% των ΜΕΘ")
 
-import numpy as np
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots(figsize=(30, 15), subplot_kw=dict(aspect="equal"))
+diag = str(input("Θέλετε να εμφαιστεί το διάγραμμα;\n\nΠατήστε 'Y' για ΝΑΙ και 'N' για ΟΧΙ: "))
 
-recipe = ['ΕΝΕΡΓΟΙ', 'ΙΑΝΣΗ', 'ΘΑΝΑΤΟΙ', 'ΚΡΙΣΙΜΑ']
-
-data = [b, c, d, e]
-
-wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
-
-bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
-kw = dict(arrowprops=dict(arrowstyle="-"),
+if diag == "Y":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots(figsize=(30, 15), subplot_kw=dict(aspect="equal"))
+    recipe = ['ΕΝΕΡΓΟΙ', 'ΙΑΝΣΗ', 'ΘΑΝΑΤΟΙ', 'ΚΡΙΣΙΜΑ']
+    data = [b, c, d, e]
+    wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
+    bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+    kw = dict(arrowprops=dict(arrowstyle="-"),
           bbox=bbox_props, zorder=0, va="center")
-
-for i, p in enumerate(wedges):
-    ang = (p.theta2 - p.theta1)/2. + p.theta1
-    y = np.sin(np.deg2rad(ang))
-    x = np.cos(np.deg2rad(ang))
-    horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
-    connectionstyle = "angle,angleA=0,angleB={}".format(ang)
-    kw["arrowprops"].update({"connectionstyle": connectionstyle})
-    ax.annotate(recipe[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
-                horizontalalignment=horizontalalignment, **kw)
-
-ax.set_title("ΕΛΛΑΔΑ")
-
-plt.show()
-
-input("\n\nΠατήστε enter για έξοδο από το πρόγραμμα..")
-
+          
+          for i, p in enumerate(wedges):
+              ang = (p.theta2 - p.theta1)/2. + p.theta1
+              y = np.sin(np.deg2rad(ang))
+              x = np.cos(np.deg2rad(ang))
+              horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+              connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+              kw["arrowprops"].update({"connectionstyle": connectionstyle})
+              ax.annotate(recipe[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+              horizontalalignment=horizontalalignment, **kw)
+              ax.set_title("ΕΛΛΑΔΑ")
+              plt.show()
+else:
+    input("\n\nΠατήστε enter για έξοδο από το πρόγραμμα..")
